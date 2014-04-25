d8f5fa6b00bec7150cc17aaba371c3a16f8c39b6
cgui@usc.edu <tedatworking@gmail.com>
2014-04-24 20:30:15 -0700

    add migration of events

---
+++ app/database/migrations/2014_04_25_032059_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+            Schema::create('events', function($table)
+            {
+                    /* @var $table \Illuminate\Database\Schema\Blueprint */
+                    $table->increments('id');
+                    $table->string('title');
+                    $table->string('description');
+                    $table->dateTime('from');
+                    $table->dateTime('to');
+                    $table->timestamps();
+                    $table->softDeletes();
+            });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+            Schema::drop('events');
+	}
+
+}
